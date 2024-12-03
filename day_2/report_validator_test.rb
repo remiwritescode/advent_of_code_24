5844f2da7bcc6a3486c7d7fff844b89623b39ef5
Remi Lapicola <remi@lapicola.com>
2024-12-02 22:06:01 -0700

    Add basic report validator spec

---
+++ day_2/report_validator_test.rb
@@ -0,0 +1,35 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'report'
+
+class ReportValidatorTest < Minitest::Test
+  def safe_report_count
+    ReportValidator.new(@reports_input).safe_count
+  end
+
+  def test_it_counts_a_single_safe_report
+    @reports_input = """
+    1 2 3
+    """
+
+    assert_equal 1, safe_report_count
+  end
+
+  def test_it_does_not_count_a_single_unsafe_report
+    @reports_input = """
+    1 2 7
+    """
+
+    assert_equal 0, safe_report_count
+  end
+
+  def test_it_counts_only_safe_reports_when_given_multiple
+    @reports_input = """
+    1 2 3
+    1 2 7
+    """
+
+    assert_equal 1, safe_report_count
+  end
+end
