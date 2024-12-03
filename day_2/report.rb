e0013fb82ec5adbd1a1a38f500eaecd683f3882b
Remi Lapicola <remi@lapicola.com>
2024-12-02 22:00:37 -0700

    Add report

---
+++ day_2/report.rb
@@ -0,0 +1,28 @@
+class Report
+  MAX_DELTA = 3
+  MIN_DELTA = 1
+  
+  def initialize(report_data)
+    @levels = report_data.split.map(&:to_i)
+  end
+
+  def safe?
+    @levels.each_cons(2) do |current_value, next_value|
+      return false unless valid_delta?(current_value, next_value)
+    end
+
+    true
+  end
+
+  private
+
+  def valid_delta?(current_value, next_value)
+    delta = next_value - current_value
+    step_direction = delta.abs == delta ? :positive : :negative
+    @report_direction ||= step_direction
+
+    return false if delta.abs < MIN_DELTA
+    return false if delta.abs > MAX_DELTA
+    @report_direction == step_direction
+  end
+end
