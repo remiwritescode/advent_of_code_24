b4581c4e74b7638d6306b09207418e91c953be9d
Remi Lapicola <remi@lapicola.com>
2024-12-07 21:44:23 -0700

    Remove input logging

--- day_7/calibration.rb
+++ day_7/calibration.rb
@@ -4,36 +4,31 @@ class Calibration
 
   def initialize(values)
     @values = values
-    initialize_signs
   end
 
   def possible_calibration?(target)
-    initialize_signs
-    i = @signs.length - 1
-    while i >= 0
-      return true if current_calibration == target
-      
-      swap_sign(i)
-      swap_sign(right_most_multiply) if current_calibration > target
-      i -= 1
+    possible_signs.any? do |sign_permutation|
+      calibration_value(sign_permutation) == target
     end
-
-    return current_calibration == target
   end
 
   private
 
-  def right_most_multiply
-    found_index = nil
-    @signs.each_with_index do |sign, idx|
-      found_index = idx if sign == MULTIPLY
+  def possible_signs
+    signs = [[ADD], [MULTIPLY]]
+    while signs.length < (@values.length - 1) * 2
+      signs = signs.each_with_object([]) do |sign_array, memo|
+        [ADD, MULTIPLY].each do |new_sign|
+          memo << sign_array + [new_sign]
+        end
+      end
     end
-    found_index
+    return signs
   end
 
-  def current_calibration
+  def calibration_value(signs)
     total = @values.first
-    @signs.each_with_index do |current_sign, idx|
+    signs.each_with_index do |current_sign, idx|
       if current_sign == ADD
         total += @values[idx + 1]
       else
@@ -42,14 +37,5 @@ class Calibration
     end
 
     total
-  end
-
-  def swap_sign(position)
-    return if position.nil?
-    @signs[position] = @signs[position] == ADD ? MULTIPLY : ADD
-  end
-
-  def initialize_signs
-    @signs = [ADD] * (@values.length - 1)
   end
 end
