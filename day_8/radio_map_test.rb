53475e1072a921db955a38611fcc7aeb960fc0b9
Remi Lapicola <remi@lapicola.com>
2024-12-07 23:58:27 -0700

    Init day 8

---
+++ day_8/radio_map_test.rb
@@ -0,0 +1,56 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative 'radio_map'
+
+class RadioMapTest < Minitest::Test
+  def antinode_count
+    map = RadioMap.new(@tower_map)
+    map.antinode_count
+  end
+
+  def test_it_can_determine_a_trivial_antinode_count
+    @tower_map = ".aa."
+
+    assert_equal 2, antinode_count
+  end
+
+  def test_it_can_determine_a_vertical_antinode
+    @tower_map = ".aa.".split('').join("\n")
+
+    assert_equal 2, antinode_count
+  end
+
+  def test_it_does_not_count_antinodes_that_fall_out_of_bounds
+    @tower_map = ".aa"
+
+    assert_equal 1, antinode_count
+  end
+
+  def test_it_can_calculate_multiple_antinodes
+    @tower_map = "....\n.OO.\n.OO.\n...."
+    # ....
+    # .OO.
+    # .OO.
+    # ....
+
+    assert_equal 12, antinode_count
+  end
+
+  def test_it_can_calculate_antinodes_from_multiple_frequencies
+    @tower_map = "....\n.:3.\n.:3.\n...."
+    # ....
+    # .:3.
+    # .:3.
+    # ....
+
+    assert_equal 4, antinode_count
+  end
+
+  def test_it_ignores_single_towers
+    @tower_map = ".a."
+
+    assert_equal 0, antinode_count
+  end
+end
